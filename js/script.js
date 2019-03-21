62602fe49559eb8ccc703014ed55867df2339f0b
Tubka <krzysiek199401@poczta.fm>
2019-03-21 21:49:00 +0100

    change js - add onload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-const hamb = document.querySelector(".navbar__burger");
-const menuList = document.querySelector(`.navbar__menu`);
+const init = function() {
+  const hamb = document.querySelector(".navbar__burger");
+  const menuList = document.querySelector(".navbar__menu");
 
-const menu = function () {
-  menuList.classList.toggle("navbar__menu--display");
+  const menu = function() {
+    menuList.classList.toggle("navbar__menu--display");
+  };
+  hamb.addEventListener("click", menu);
 };
-hamb.addEventListener("click", menu);
+
+window.onload = function() {
+  init();
+};
